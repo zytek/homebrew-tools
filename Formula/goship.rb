3e3ba860b73e37133a6a553a894093318d53c8ec
Jakub Paweł Głazik <zytek@nuxi.pl>
2020-04-10 14:35:22 +0200

    goship: add version

--- Formula/goship.rb
+++ Formula/goship.rb
@@ -3,6 +3,7 @@ class Goship < Formula
   homepage "https://github.com/zendesk/goship"
   url "https://github.com/zytek/goship/archive/1.0.5-ec2.tar.gz"
   sha256 "8108f04ffe5643ab6a6735a1d83e95026ac68da8d0686276ecec3a4ed7dbbae7"
+  version "1.0.5-ec2"
 
   depends_on "go" => :build
 
