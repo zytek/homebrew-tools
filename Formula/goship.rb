7e606c74eda47fbc0bfa299a9f44e31a743e80e4
Jakub Paweł Głazik <zytek@nuxi.pl>
2020-04-10 14:30:19 +0200

    Add goship Formula

---
+++ Formula/goship.rb
@@ -0,0 +1,17 @@
+class Goship < Formula
+  desc "Utility that helps find, connect  and copy to particular cloud resources using configured providers"
+  homepage "https://github.com/zendesk/goship"
+  url "https://github.com/zytek/goship/archive/1.0.5-ec2.tar.gz"
+  sha256 "8108f04ffe5643ab6a6735a1d83e95026ac68da8d0686276ecec3a4ed7dbbae7"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", "goship"
+    bin.install "goship"
+  end
+
+  test do
+    system "#{bin}/goship version"
+  end
+end
---
+++ README.md
@@ -0,0 +1,15 @@
+# Zytek Tools
+
+## How do I install these formulae?
+`brew install zytek/tools/<formula>`
+
+Or `brew tap zytek/tools` and then `brew install <formula>`.
+
+Or install via URL (which will not receive updates):
+
+```
+brew install https://raw.githubusercontent.com/zytek/homebrew-tools/master/Formula/<formula>.rb
+```
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
